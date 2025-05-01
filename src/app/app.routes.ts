34c44aac07f4ea5c2bdcc6b503ec06e5c7dabd7b
daicalong <hpnguyen52@gmail.com>
2025-05-01 18:35:30 +0000

    added more lifecycle examples

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,16 @@ import { Route } from "@angular/router";
 
 export const appRoutes: Route[] = [
     {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        title: 'Home',
+        path: 'home',
+        loadComponent: () => import('../home/home.component').then(c => c.HomeComponent)
+    },
+    {
         title: 'Lifecycle Example',
         path: 'lifecycle-example',
         loadComponent: () => import('../lifecycle-example/lifecycle-example.component').then(c => c.LifecycleExampleComponent)
